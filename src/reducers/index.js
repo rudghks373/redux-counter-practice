186844dc46e3a372c0efb84137f2f8b0d81a032a
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-03 15:15:41 +0900

    Updata V 2.0
    멀티카운터 리덕스 적용
    생성,삭제 버튼 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,83 @@
+/* 
+쪼개기
+
+import number from "./number";
+import color from "./color";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  numberData: number,
+  colorData: color
+});
+
+export default reducers;
+
+*/
+
 import * as types from "../actions/ActionTypes";
 
-//초기 상태를 정의한다
+// 초기 상태를 정의합니다.
 const initialState = {
-  color: "black",
-  number: 0
+  counters: [
+    {
+      color: "black",
+      number: 0
+    }
+  ]
 };
 
 function counter(state = initialState, action) {
+  // 레퍼런스 생성
+  const { counters } = state;
+
   switch (action.type) {
+    case types.CREATE:
+      return {
+        counters: [
+          ...counters,
+          {
+            color: action.color,
+            number: 0
+          }
+        ]
+      };
+    case types.REMOVE:
+      return {
+        counters: counters.slice(0, counters.length - 1)
+      };
     case types.INCREMENT:
       return {
-        ...state,
-        number: state.number + 1
+        counters: [
+          ...counters.slice(0, action.index), // 선택한 인덱스의 전 아이템들
+          {
+            ...counters[action.index], // 기존 객체에
+            number: counters[action.index].number + 1 // 새 number 값 덮어쓰기
+          },
+          ...counters.slice(action.index + 1, counters.length) // 선택한 인덱스의 다음 아이템들
+        ]
       };
     case types.DECREMENT:
       return {
-        ...state,
-        number: state.number - 1
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number - 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
       };
     case types.SET_COLOR:
       return {
-        ...state,
-        color: action.color
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            color: action.color
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
       };
     default:
       return state;
