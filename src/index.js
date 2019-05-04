393033a14eaae1ebf939ad42841f78df5fea5030
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-04 17:39:31 +0900

    Updata index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import { createStore } from "redux";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
-
+//리덕스 생성
 const store = createStore(reducers);
 
 ReactDOM.render(
