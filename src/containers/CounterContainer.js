32db7d18d3504858d1e49c9f6e12d052064d9fb0
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-05 12:54:49 +0900

    Updata subject

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -2,7 +2,7 @@ import Counter from "../components/Counter";
 import * as actions from "../actions";
 import { connect } from "react-redux";
 
-// 13가지 색상 중 랜덤으로 선택하는 함수입니다.
+// 13가지 색상 중 랜덤으로 선택하는 함수입니다..
 export function getRandomColor() {
   const colors = [
     "#495057",
