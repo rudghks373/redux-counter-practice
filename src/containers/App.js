5d242526b048380e2196bbed1a5d846ef35cd3ed
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-15 16:33:15 +0900

    Updata App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Buttons from "../components/Buttons";
 import CounterListContainer from "./CounterListContainer";
 import getRandomColor from "../lib/getRandomColor";
-
 import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class App extends Component {
   render() {
@@ -17,7 +17,7 @@ class App extends Component {
   }
 }
 
-// 액션 생성 함수 준비
+// 액션 생성 함수 준비.
 const mapToDispatch = dispatch => ({
   onCreate: () => dispatch(actions.create(getRandomColor())),
   onRemove: () => dispatch(actions.remove())
